6e5ef9d9c2646ef42a4e6b22ae56949820c19e8b
aslisubhash <myearning4me@gmail.com>
2023-04-05 10:31:22 +0530

    first commit

---
+++ example/upi_payment_flutter_example.dart
@@ -0,0 +1,6 @@
+import 'package:upi_payment_flutter/upi_payment_flutter.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/upi_payment_handler.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class UpiPaymentHandler {
+  static const _upiUrlScheme = 'upi://pay';
+
+  Future<bool> initiateTransaction({
+    required String payeeVpa,
+    required String payeeName,
+    required String transactionRefId,
+    required String transactionNote,
+    required double amount,
+  }) async {
+    String url = _createUpiUrl(
+      payeeVpa: payeeVpa,
+      payeeName: payeeName,
+      transactionRefId: transactionRefId,
+      transactionNote: transactionNote,
+      amount: amount,
+    );
+
+    if (await canLaunch(url)) {
+      await launch(url);
+      return true;
+    } else {
+      throw PlatformException(
+          code: 'UPI_NOT_SUPPORTED',
+          message: 'UPI payment is not supported on this device.');
+    }
+  }
+
+  String _createUpiUrl({
+    required String payeeVpa,
+    required String payeeName,
+    required String transactionRefId,
+    required String transactionNote,
+    required double amount,
+  }) {
+    return Uri(
+      scheme: _upiUrlScheme,
+      queryParameters: {
+        'pa': payeeVpa,
+        'pn': payeeName,
+        'tid': transactionRefId,
+        'tn': transactionNote,
+        'am': amount.toStringAsFixed(2),
+        'cu': 'INR',
+      },
+    ).toString();
+  }
+}
